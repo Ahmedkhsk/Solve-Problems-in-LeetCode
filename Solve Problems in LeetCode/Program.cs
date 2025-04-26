401358d9e01869f1795eac25d43da310a240bd2e
Ahmedkhsk <mrahmedkhaled2004@gmail.com>
2025-04-26 20:56:05 +0300

    Solve Add Two Numbers with Linked List

--- Solve Problems in LeetCode/Program.cs
+++ Solve Problems in LeetCode/Program.cs
@@ -1,25 +1,61 @@
-﻿//Two Sum
+﻿//Add Two Numbers with Linked List
 
-int[] TwoSum(int[] nums , int target)
+public class ListNode
 {
-    int[] result = new int[2];
-
-    for (int i = 0; i < nums.Length; i++)
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
     {
-        for(int j = i+1; j<nums.Length;j++)
-        {
-            if (nums[i] + nums[j] == target)
-            {
-                result[0] = i;
-                result[1] = j;
-                return result;
-            }
-        }
+        this.val = val;
+        this.next = next;
     }
-    return result;
 }
 
-int[] nums = { 2, 7, 11, 15 };
-int target = 9;
-int[] result = TwoSum(nums, target);
-Console.WriteLine($"Indices: {result[0]}, {result[1]}");
+public class Solution
+{
+    public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0)
+        {
+            int sum = carry;
+            if(l1 != null)
+            {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null)
+            {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+
+        }
+        return dummyHead.next;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        ListNode result = Solution.AddTwoNumbers(l1, l2);
+
+        while (result != null)
+        {
+            Console.Write(result.val + " ");
+            result = result.next;
+        }
+    }
+}
+
+
