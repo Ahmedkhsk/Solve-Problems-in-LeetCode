8ea2e444ab6945f23deb5c73373c8e023181193d
Ahmedkhsk <mrahmedkhaled2004@gmail.com>
2025-04-26 20:25:56 +0300

    Solve Two Sum

--- Solve Problems in LeetCode/Program.cs
+++ Solve Problems in LeetCode/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Solve in LeetCode");
+﻿//Two Sum
+
+int[] TwoSum(int[] nums , int target)
+{
+    int[] result = new int[2];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for(int j = i+1; j<nums.Length;j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                result[0] = i;
+                result[1] = j;
+                return result;
+            }
+        }
+    }
+    return result;
+}
+
+int[] nums = { 2, 7, 11, 15 };
+int target = 9;
+int[] result = TwoSum(nums, target);
+Console.WriteLine($"Indices: {result[0]}, {result[1]}");
