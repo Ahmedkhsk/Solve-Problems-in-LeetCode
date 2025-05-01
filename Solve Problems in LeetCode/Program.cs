dd7a860517f8b82df263e7065329be37e5e6f918
Ahmedkhsk <mrahmedkhaled2004@gmail.com>
2025-05-01 23:59:30 +0300

    Maximum Number Of Tasks You Can Assign

--- Solve Problems in LeetCode/Program.cs
+++ Solve Problems in LeetCode/Program.cs
@@ -1,36 +1,81 @@
-﻿//Find Numbers with Even Number of Digits
-//https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/
+﻿//Maximum Number Of Tasks You Can Assign
+//https://leetcode.com/problems/maximum-number-of-tasks-you-can-assign/description
 public class Solution
 {
-    public int FindNumbers(int[] nums)
+    public int MaxTaskAssign(int[] tasks, int[] workers, int pills, int strength)
     {
-        int count = 0;
-        foreach(var num in nums)
+        Array.Sort(tasks);
+        Array.Sort(workers);
+
+        int left = 0, right = Math.Min(tasks.Length, workers.Length), result = 0;
+
+        while (left <= right)
         {
-            int digitCount = 0, n = num;
-            while(n > 0)
+            int mid = (left + right) / 2;
+            if (CanAssign(tasks, workers, pills, strength, mid))
             {
-                n /= 10;
-                digitCount++;
+                result = mid;
+                left = mid + 1;
             }
-            if (digitCount % 2 == 0)
+            else
             {
-                count++;
+                right = mid - 1;
             }
         }
-        return count;
+
+        return result;
     }
 
+    private bool CanAssign(int[] tasks, int[] workers, int pills, int strength, int k)
+    {
+        var taskQueue = new Queue<int>(tasks.Take(k).OrderByDescending(t => t));
+        var workerList = new List<int>(workers.TakeLast(k)); 
+
+        int pillCount = pills;
+
+        while (taskQueue.Count > 0)
+        {
+            int task = taskQueue.Dequeue();
+
+            if (workerList.Count == 0) return false;
+
+            if (workerList[workerList.Count - 1] >= task)
+            {
+                workerList.RemoveAt(workerList.Count - 1);
+            }
+            else
+            {
+                if (pillCount == 0) return false;
+
+                int index = workerList.FindIndex(w => w + strength >= task);
+                if (index == -1) return false;
+
+                workerList.RemoveAt(index);
+                pillCount--;
+            }
+        }
+
+        return true;
+    }
 }
+
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
-        Solution solution = new Solution();
-              
+        var sol = new Solution();
+        int[] tasks = { 10, 15, 30 };
+        int[] workers = { 0, 10, 10, 10, 10 };
+        int pills = 3;
+        int strength = 10;
+
+        Console.WriteLine($"Count of Tasks => {sol.MaxTaskAssign(tasks, workers, pills, strength)}"); 
+
+        Console.ReadLine();
     }
 }
+
 
 
 
