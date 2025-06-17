e5ad3f88c25ca32ac15dc992f9c61cb1c7e50570
Ahmedkhsk <mrahmedkhaled2004@gmail.com>
2025-06-17 07:38:23 +0300

    Count the Number of Arrays with K Matching Adjacent Elements

--- Solve Problems in LeetCode/Program.cs
+++ Solve Problems in LeetCode/Program.cs
@@ -1,48 +1,67 @@
-﻿//Count Largest Group
-//https://leetcode.com/problems/count-largest-group/description
+﻿//Count the Number of Arrays with K Matching Adjacent Elements
+//https://leetcode.com/problems/count-the-number-of-arrays-with-k-matching-adjacent-elements/description
 using System;
 using System.Linq;
 using System.Collections.Generic;
 
 public class Solution
 {
-    public int CountLargestGroup(int n)
+    const int MOD = 1_000_000_007;
+    const int MAX = 100005;
+
+    long[] fact = new long[MAX];
+    long[] invFact = new long[MAX];
+
+    public Solution()
     {
-        Dictionary<int, int> groupCounts = new Dictionary<int, int>();
+        fact[0] = invFact[0] = 1;
 
-        for (int i = 1; i <= n; i++)
-        {
-            int sum = 0;
-            int num = i;
+        for (int i = 1; i < MAX; i++)
+            fact[i] = (fact[i - 1] * i) % MOD;
 
-            while (num > 0)
-            {
-                sum += num % 10;
-                num /= 10;
-            }
-
-            if (groupCounts.ContainsKey(sum))
-                groupCounts[sum]++;
-            else
-                groupCounts[sum] = 1;
-        }
-
-        int maxSize = groupCounts.Values.Max();
-
-        int count = groupCounts.Values.Count(v => v == maxSize);
-
-        return count;
+        invFact[MAX - 1] = ModInverse(fact[MAX - 1]);
+        for (int i = MAX - 2; i >= 1; i--)
+            invFact[i] = (invFact[i + 1] * (i + 1)) % MOD;
     }
 
+    long ModInverse(long x) => ModPow(x, MOD - 2);
+
+    long ModPow(long a, int b)
+    {
+        long res = 1;
+        while (b > 0)
+        {
+            if ((b & 1) == 1)
+                res = (res * a) % MOD;
+            a = (a * a) % MOD;
+            b >>= 1;
+        }
+        return res;
+    }
+
+    long Comb(int n, int k)
+    {
+        if (k < 0 || k > n) return 0;
+        return (((fact[n] * invFact[k]) % MOD) * invFact[n - k]) % MOD;
+    }
+
+    public int CountGoodArrays(int n, int m, int k)
+    {
+        long comb = Comb(n - 1, k);
+        long pow = ModPow(m - 1, n - 1 - k);
+        long result = ((comb * pow) % MOD * m) % MOD;
+        return (int)result;
+    }
 }
+
 public class Program
 {
     public static void Main()
     {
         var sol = new Solution();
-        int num = 13;
+        int n = 4, m = 2, k = 2;
 
-        Console.WriteLine(sol.CountLargestGroup(num));
+        Console.WriteLine(sol.CountGoodArrays(n,m,k));
 
 
     }
