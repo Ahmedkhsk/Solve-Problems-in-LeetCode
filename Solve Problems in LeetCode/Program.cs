579438bb5fbd88f9d35bfb1394ee13282906b0ad
Ahmedkhsk <mrahmedkhaled2004@gmail.com>
2025-06-18 10:12:44 +0300

    Divide Array Into Arrays With Max Difference

--- Solve Problems in LeetCode/Program.cs
+++ Solve Problems in LeetCode/Program.cs
@@ -1,29 +1,52 @@
-﻿//Valid Anagram
-//https://neetcode.io/problems/is-anagram?list=neetcode150
-using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Globalization;
+﻿//Divide Array Into Arrays With Max Difference
+//https://leetcode.com/problems/divide-array-into-arrays-with-max-difference/description/
+
 
 public class Solution
 {
-    public bool IsAnagram(string s, string t)
+    public int[][] DivideArray(int[] nums, int k)
     {
-        string sortedS = new string(s.OrderBy(c => c).ToArray());
-        string sortedT = new string(t.OrderBy(c => c).ToArray());
+        Array.Sort(nums);
+        int n = nums.Length;
+        List<int[]> result = new List<int[]>();
 
-        return sortedS.SequenceEqual(sortedT);
-        
+        for (int i = 0; i < n; i += 3)
+        {
+            int a = nums[i];
+            int b = nums[i + 1];
+            int c = nums[i + 2];
+
+            if (c - a > k)
+            {
+                return new int[0][];
+            }
+
+            result.Add(new int[] { a, b, c });
+        }
+
+        return result.ToArray();
     }
 }
+
 public class Program
 {
     public static void Main()
     {
         Solution solution = new Solution();
 
-        Console.WriteLine(solution.IsAnagram("racecar", "carrace"));
+        int[][] arr = solution.DivideArray(new int[] { 1, 2, 3, 4, 5, 6 }, 2);
+
+        if (arr.Length == 0)
+        {
+            Console.WriteLine("[]");
+        }
+        else
+        {
+            foreach (var subArray in arr)
+            {
+                Console.WriteLine($"[{string.Join(", ", subArray)}]");
+            }
+        }
 
     }
 }
